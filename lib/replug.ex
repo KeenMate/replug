a49d8e3485647c83482bd44e3d3418d2b6365576
Filip Jakab <jakabfilip.fj@gmail.com>
2022-03-29 14:10:59 +0200

    Slight refactoring and added support for plug functions with static opts as well

--- lib/replug.ex
+++ lib/replug.ex
@@ -32,13 +32,13 @@ defmodule Replug do
   end
 
   @impl true
-  def call(conn, %{plug: {:fn, plug_function, :only_dynamic_opts}, opts: {opts_module, opts_function}}) do
-    opts = apply(opts_module, opts_function, [])
+  # def call(conn, %{plug: {:fn, plug_function, :only_dynamic_opts}, opts: {opts_module, opts_function}}) do
+  #   opts = apply(opts_module, opts_function, [])
 
-    plug_function.(conn, opts)
-  end
+  #   plug_function.(conn, opts)
+  # end
 
-  def call(conn, %{plug: {:mod, plug_module, :only_dynamic_opts}, opts: {opts_module, opts_function}}) do
+  def call(conn, %{plug: {plug_type, plug_module, :only_dynamic_opts}, opts: {opts_module, opts_function}}) do
     opts =
       opts_module
       |> apply(opts_function, [])
@@ -58,33 +58,18 @@ defmodule Replug do
     plug_module.call(conn, opts)
   end
 
-  def call(conn, %{plug: {plug_module, :only_dynamic_opts}, opts: {opts_module, opts_function, opt_args}}) do
-    opts =
-      opts_module
-      |> apply(opts_function, [opt_args])
-      |> plug_module.init()
+  def call(conn, %{plug: {plug_type, plug_module, plug_opts}, opts: opts}) do
+    opts = build_plug_opts(plug_type, plug_module, plug_opts, opts)
 
+    call_plug(conn, plug_type, plug_module, opts)
+  end
+
+  defp call_plug(conn, :fn, plug_fn, opts) do
+    plug_fn.(conn, opts)
+  end
+
+  defp call_plug(conn, :mod, plug_module, opts) do
     plug_module.call(conn, opts)
-  end
-
-  def call(conn, %{plug: {plug_module, static_opts}, opts: {opts_module, opts_function, opt_args}}) do
-    dynamic_opts = apply(opts_module, opts_function, [opt_args])
-
-    opts =
-      static_opts
-      |> merge_opts(dynamic_opts)
-      |> plug_module.init()
-
-    plug_module.call(conn, opts)
-  end
-
-  defp merge_opts(static_opts, dynamic_opts)
-       when is_list(static_opts) and is_list(dynamic_opts) do
-    Keyword.merge(static_opts, dynamic_opts)
-  end
-
-  defp merge_opts(static_opts, dynamic_opts) when is_map(static_opts) and is_map(dynamic_opts) do
-    Map.merge(static_opts, dynamic_opts)
   end
 
   defp parse_plug_opt(opts) do
@@ -98,12 +83,44 @@ defmodule Replug do
       plug_module when is_atom(plug_module) or is_function(plug_module) ->
         {plug_module, :only_dynamic_opts}
     end
-    |> case do
-      {plug_function, opts} when is_function(plug_function) ->
-        {:fn, plug_function, opts}
+    |> plug_with_type()
+  end
 
-      {plug_module, opts} ->
-        {:mod, plug_module, opts}
+  defp plug_with_type({plug_param, opts}) when is_function(plug_param) do
+    {:fn, plug_param, opts}
+  end
+
+  defp plug_with_type({plug_param, opts}) when is_atom(plug_param) do
+    {:mod, plug_param, opts}
+  end
+
+  defp build_plug_opts(plug_type, plug_module, plug_opts, {opts_module, opts_function, opt_args}) do
+    dynamic_opts = apply(opts_module, opts_function, opt_args)
+
+    case plug_opts do
+      :only_dynamic_opts ->
+        dynamic_opts
+
+      static_opts ->
+        merge_opts(static_opts, dynamic_opts)
     end
+    |> maybe_plug_init()
+  end
+
+  defp maybe_plug_init(opts, :fn, _plug_fn) do
+    opts
+  end
+
+  defp maybe_plug_init(opts, :mod, plug_mod) do
+    plug_mod.init(opts)
+  end
+
+  defp merge_opts(static_opts, dynamic_opts)
+       when is_list(static_opts) and is_list(dynamic_opts) do
+    Keyword.merge(static_opts, dynamic_opts)
+  end
+
+  defp merge_opts(static_opts, dynamic_opts) when is_map(static_opts) and is_map(dynamic_opts) do
+    Map.merge(static_opts, dynamic_opts)
   end
 end
